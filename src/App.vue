7ebdb9a77111d9a91aeb1ec65df86d1f03a3276a
orkhan.verdiev <orkhan.verdiev@cibirlan.com>
2023-04-08 11:55:39 +0200

    fix: remove scss

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,13 +20,13 @@
 #nav {
   padding: 30px;
 
-  a {
+  /* a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
       color: #42b983;
     }
-  }
+  } */
 }
 </style>
