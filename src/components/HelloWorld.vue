edc7fd401fa5e1931379052b39463a0ef0dbb347
orkhan.verdiev <orkhan.verdiev@cibirlan.com>
2023-04-26 20:45:18 +0200

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,32 +3,17 @@
     <v-btn @click="testApi">
       Тест api
     </v-btn>
-    <canvas
-        width="1000"
-        height="600"
-        class="canvas-grid"
-        ref="canvasGrid"
-    >
+    <v-btn @click="testPostApi">
+      Тест post api
+    </v-btn>
+    <canvas width="1000" height="600" class="canvas-grid" ref="canvasGrid">
     </canvas>
     <v-container>
       <v-carousel v-model="model">
-        <v-carousel-item
-            v-for="(color, i) in colors"
-            :key="color"
-        >
-          <v-sheet
-              :color="color"
-              height="100%"
-              tile
-          >
-            <v-row
-                class="fill-height"
-                align="center"
-                justify="center"
-            >
-              <div class="text-h2">
-                Slide {{ i + 1 }}
-              </div>
+        <v-carousel-item v-for="(color, i) in colors" :key="color">
+          <v-sheet :color="color" height="100%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h2">Slide {{ i + 1 }}</div>
             </v-row>
           </v-sheet>
         </v-carousel-item>
@@ -38,58 +23,68 @@
 </template>
 
 <script>
-import io from "socket.io"
-import axios from 'axios'
+import io from "socket.io";
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data: () => ({
     model: 0,
-    colors: [
-      'primary',
-      'secondary',
-      'yellow darken-2',
-      'red',
-      'orange',
-    ],
+    colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
   }),
   mounted() {
     this.src();
-    this.buildGrid()
+    this.buildGrid();
   },
   methods: {
-    buildGrid () {
-      const context = this.$refs.canvasGrid.getContext('2d')
+    buildGrid() {
+      const context = this.$refs.canvasGrid.getContext("2d");
       for (let x = 0.5; x < 800; x += 50) {
-        context.moveTo(x, 0)
-        context.lineTo(x, 800)
+        context.moveTo(x, 0);
+        context.lineTo(x, 800);
       }
 
       for (let y = 0.5; y < 800; y += 50) {
-        context.moveTo(0, y)
-        context.lineTo(800, y)
+        context.moveTo(0, y);
+        context.lineTo(800, y);
       }
 
-      context.lineWidth = 1
-      context.globalAlpha = 1
-      context.strokeStyle = "rgba(255, 0, 0, 1)"
-      context.stroke()
+      context.lineWidth = 1;
+      context.globalAlpha = 1;
+      context.strokeStyle = "rgba(255, 0, 0, 1)";
+      context.stroke();
     },
     src() {
-      let recaptchaScript = document.createElement('script');
-      recaptchaScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js');
+      let recaptchaScript = document.createElement("script");
+      recaptchaScript.setAttribute(
+        "src",
+        "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"
+      );
       document.head.appendChild(recaptchaScript);
     },
     testApi() {
-        axios.post('https://arduino-back-production.up.railway.app/', {
-          mode: 'no-cors',
+      axios
+        .get("http://localhost:3000/", {
+          mode: "no-cors",
         })
-            .then((response) => {
-              console.log(response.data);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-      }
-  }
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    testPostApi() {
+      const data = { name: "John Doe", email: "johndoe@example.com" };
+
+      axios
+        .post("http://localhost:3000/users", data)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>
